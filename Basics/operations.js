cd87981d9e08b3b01ee28c49ab83d27b33183915
Divya Kaushik <162818410+divya-kaushikkk@users.noreply.github.com>
2025-07-23 13:29:19 +0000

    update operations.js

---
+++ Basics/operations.js
@@ -0,0 +1,19 @@
+let value = 3
+let negValue = -value
+console.log(value);    // 3
+console.log(negValue); // -3
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+console.log(str3);   // Hello World
+
+console.log("1" + 2);  // 12
+
+console.log("1" + "2"); // 12
+
+console.log(1 + "2");   // 12
+
+console.log(1 + 2 + "2"); // 32
+
+console.log("1" + 2 + 2);  // 122
