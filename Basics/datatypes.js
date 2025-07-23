3cf749bac3895be608ac8ad41e2b6467f07ac7cd
Divya Kaushik <162818410+divya-kaushikkk@users.noreply.github.com>
2025-07-23 13:22:34 +0000

    update datatypes.js

--- Basics/datatypes.js
+++ Basics/datatypes.js
@@ -1,7 +1,7 @@
-let name = "Divya"    //String
+let name1 = "Divya"    //String
 let age = 19          //Number 
 let isLoggedIn = true //Boolean
-console.log(typeof(name));
+console.log(typeof(name1));
 console.log(typeof age);
 console.log(typeof true);
 
@@ -10,9 +10,14 @@ console.log(typeof undefined);  //undefined
 
 let score = "33"
 console.log(typeof score)    //string
-let valScore = Number(score)
-console.log(typeof valScore)  //number
+let valScore1 = Number(score)
+console.log(typeof valScore1)  //number
 
 
+let name2 = 33
+console.log(typeof name2)  // number
+
+let valScore2 = String(name2)
+console.log(typeof valScore2)  // string
 
 
